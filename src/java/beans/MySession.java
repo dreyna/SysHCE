1559f27e2034a4f1f42afb494ffd22bd98e34bc2
alum.fial1 <alum.fial1@FIA-LAB1C5.lim.upeu.edu.pe>
2015-07-12 12:00:10 -0500

    v6

---
+++ src/java/beans/MySession.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author DReyna
+ */
+@ManagedBean
+@SessionScoped
+public class MySession {
+
+    /**
+     * Creates a new instance of MySession
+     */
+    public MySession() {
+    }
+    
+}
